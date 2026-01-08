c1daabd0fcba3dcad13df63ed9288890cd0fa4c8
Boomesh <boomesh2k@gmail.com>
2026-01-08 19:36:08 +0530

    "first commit"

---
+++ patient-service/src/main/java/org/example/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,21 @@
+package org.example.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank(message = "Name is required   ")
+    @Size(max = 100,message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotNull
+}
